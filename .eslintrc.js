6479bda4c0ef856c603182b7f8779f5d726d6fab
larissa <oliveir5uwm@gmail.com>
2022-06-14 00:54:33 -0300

    build: sets the basic node project structure

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'space-before-function-paren': 'off',
+    'space-before-blocks': 'off',
+    semi: 'off',
+    quotes: 'off'
+  }
+}
